dcbcd8f57a4eecef5dd2b015e1a5caf5422e650c
Mehmet Öztemel <mehmet-oztemel@hotmail.com>
2024-05-20 02:16:21 +0300

    Create ValuesController.cs

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Net8_JWT.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Message = "API is working" });
+        }
+    }
+}
